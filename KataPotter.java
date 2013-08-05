cc64230f104c47a9b4f01b9f575fb4da09f71f21
laoniu85 <laoniu85@163.com>
2013-08-05 20:59:07 +0800

    KataPotter

---
+++ KataPotter.java
@@ -0,0 +1,454 @@
+package CodeJam2008PracticeContest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class KataPotter {
+
+	/**
+	 * 购物车类
+	 * 
+	 * @author Laoniu
+	 * 
+	 */
+
+	static public class BookCart {
+		ArrayList<Book> books;
+		int totalBooks;
+
+		public BookCart() {
+			books = new ArrayList<Book>();
+		}
+
+		public void addBook(Book book) {
+			totalBooks += book.count;
+			books.add(book);
+		}
+
+		/**
+		 * 取出一本书
+		 * 
+		 * @param book
+		 * @return
+		 */
+		boolean pickOneBook(Book book) {
+			for (Book bk : books) {
+				if (bk.name.equals(book.name)) {
+					bk.count -= 1;
+					totalBooks -= 1;
+					if (bk.count == 0) {
+						books.remove(bk);
+					}
+
+					return true;
+				}
+			}
+			return false;
+
+		}
+
+		/**
+		 * 为去重写了一个一次删除多个的方法
+		 * 
+		 * @param index
+		 * @return
+		 */
+		boolean removeFromIndex(int index) {
+			if (index >= books.size()) {
+				return false;
+			} else {
+				for (int i = books.size() - 1; i >= index; i--) {
+					totalBooks -= books.get(i).count;
+					books.remove(i);
+				}
+				return true;
+			}
+		}
+
+		public BookCart copy() {
+			BookCart ret = new BookCart();
+			for (Book bk : this.books) {
+				ret.addBook(bk.copy());
+			}
+			return ret;
+		}
+	}
+
+	/**
+	 * 书籍类
+	 * 
+	 * @author Laoniu
+	 * 
+	 */
+	static public class Book {
+		String name;
+		int count;
+
+		public Book(String in_name, int in_count) {
+			name = new String(in_name);
+			count = in_count;
+		}
+
+		public Book copy() {
+			return new Book(this.name, count);
+		}
+
+	}
+
+	/**
+	 * 书籍打包(可优惠故打包)
+	 * 
+	 * @author Laoniu
+	 * 
+	 */
+	static public class BookPackage {
+		ArrayList<Book> books;
+
+		public BookPackage() {
+			books = new ArrayList<KataPotter.Book>();
+		}
+
+		public BookPackage copy() {
+			BookPackage pkg = new BookPackage();
+
+			for (Book book : this.books) {
+				pkg.add(book.copy());
+			}
+			return pkg;
+		}
+
+		public void add(Book book) {
+			books.add(book);
+		}
+	}
+
+	static public class PackegeCombo {
+
+		ArrayList<BookPackage> BookPackages;
+		int totalPrice;
+
+		public PackegeCombo() {
+			BookPackages = new ArrayList<KataPotter.BookPackage>();
+		}
+
+		public void add(BookPackage pkg) {
+			BookPackages.add(pkg);
+		}
+
+		public PackegeCombo copy() {
+			PackegeCombo pkgcmb = new PackegeCombo();
+			for (BookPackage pkg : BookPackages) {
+				pkgcmb.add(pkg.copy());
+			}
+			return pkgcmb;
+		}
+	}
+
+	/**
+	 * 优惠策略
+	 * 
+	 * @author Laoniu
+	 * 
+	 */
+	static public class Strategy {
+		int bookCount;
+		int discountRate;
+
+		public Strategy(int count, int in_rate) {
+			bookCount = count;
+			discountRate = in_rate;
+		}
+
+		int getStrategyPrice() {
+			return bookCount * discountRate;
+		}
+
+		public Strategy copy() {
+			return new Strategy(this.bookCount, this.discountRate);
+
+		}
+	}
+
+	/**
+	 * 优惠组合 针对于一个购物车有多种优惠组合
+	 * 
+	 * @author Laoniu
+	 * 
+	 */
+	static public class StategyCombo {
+		ArrayList<Strategy> strategies;// 优惠组合
+		int totalPrice;
+
+		public StategyCombo() {
+			strategies = new ArrayList<KataPotter.Strategy>();
+			totalPrice = 0;
+		}
+
+		public void add(Strategy stategy) {
+			// TODO Auto-generated method stub
+			strategies.add(stategy);
+			totalPrice += stategy.getStrategyPrice();
+		}
+	}
+
+	public KataPotter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 此优惠组合可以使用的打包方式
+	 * 
+	 * @return
+	 */
+	public ArrayList<PackegeCombo> pickAblePakegeComboWithStategyComboFromBookCart(
+			BookCart BookCart, StategyCombo strategyCombo) {
+
+		ArrayList<BookCart> BookCarts = new ArrayList<BookCart>();
+		ArrayList<PackegeCombo> pkgcombos = new ArrayList<PackegeCombo>();
+		BookCarts.add(BookCart.copy());
+
+		for (int i = 0; i < strategyCombo.strategies.size(); i++) {
+			ArrayList<BookCart> newBookCarts = new ArrayList<BookCart>();
+			ArrayList<PackegeCombo> newPkgCombos = new ArrayList<PackegeCombo>();
+			for (int j = BookCarts.size() - 1; j >= 0; j--) {
+				ArrayList<BookPackage> pkgs = pickablePackegeFromBookCart(
+						BookCarts.get(j),
+						strategyCombo.strategies.get(i).bookCount);
+				// 剪除没有结果的分支
+				if (pkgs == null || pkgs.size() == 0) {
+					BookCarts.remove(j);
+				} else {
+					// BookCart分裂为pkgs个分支 newPkgCombos和BookCarts同时分裂
+					for (int k = 0; k < pkgs.size(); k++) {
+						// 从分支对应的
+						BookCart newCart = pickPackegeFromBookCart(BookCarts
+								.get(j).copy(), pkgs.get(k));
+
+						if (newCart == null) {
+							continue;
+						}
+						newBookCarts.add(newCart);
+						PackegeCombo pkgcmb = (i == 0) ? new PackegeCombo()
+								: pkgcombos.get(j).copy();
+						;
+						pkgcmb.add(pkgs.get(k));
+						pkgcmb.totalPrice = strategyCombo.totalPrice;
+						newPkgCombos.add(pkgcmb);
+
+					}
+				}
+
+			}
+			// 购物车无法再操作了 推出程序
+			if (newBookCarts.size() == 0) {
+				return null;// 表示没有结果
+			}
+			BookCarts = newBookCarts;
+			pkgcombos = newPkgCombos;
+		}
+		return pkgcombos;
+	}
+
+	/**
+	 * 按规定数量可从购物车中取出的书籍优惠包
+	 * 
+	 * @param BookCart
+	 * @param BookPackageCount(有几本书)
+	 * @return
+	 */
+	public ArrayList<BookPackage> pickablePackegeFromBookCart(
+			BookCart BookCart, int BookPackageCount) {
+		ArrayList<BookCart> BookCarts = new ArrayList<BookCart>();
+		BookCarts.add(BookCart.copy());
+		ArrayList<BookPackage> pkgs = new ArrayList<BookPackage>();
+		if (BookCart.books.size() < BookPackageCount)//
+		{
+			return null;
+		}
+		for (int i = 0; i < BookPackageCount; i++) {
+			ArrayList<BookCart> newBookCarts = new ArrayList<BookCart>();
+			ArrayList<BookPackage> newpkgs = new ArrayList<BookPackage>();
+			for (int j = BookCarts.size() - 1; j >= 0; j--) {
+				// 剩余需要挑选的种类大于购物车里的剩余种类则退出
+				if (BookPackageCount - i > BookCarts.get(j).books.size()) {
+					BookCarts.remove(j);
+				} else {
+
+					for (int k = 0; k < BookCarts.get(j).books.size(); k++) {
+						BookCart newBookCart = BookCarts.get(j).copy();
+						// 为去重 取出一个元素时会把它之后的元素全部取出
+						newBookCart.removeFromIndex(k);
+						BookPackage pkg = (i == 0) ? (new BookPackage()) : pkgs
+								.get(j).copy();
+						Book book = BookCarts.get(j).books.get(k).copy();
+						book.count = 1;
+						pkg.add(book);
+						newpkgs.add(pkg);
+						newBookCarts.add(newBookCart);
+					}
+				}
+
+			}
+			// 购物车无法再操作了 推出程序
+			if (newBookCarts.size() == 0) {
+				return null;// 表示没有结果
+			}
+			BookCarts = newBookCarts;
+			pkgs = newpkgs;
+		}
+
+		return pkgs;
+	}
+
+	/**
+	 * 从购物车中取出
+	 * 
+	 * @param BookCart
+	 * @param pkg
+	 * @return
+	 */
+	public BookCart pickPackegeFromBookCart(BookCart in_BookCart,
+			BookPackage pkg) {
+		BookCart bkCart = in_BookCart.copy();
+		for (Book book : pkg.books) {
+			if (false == bkCart.pickOneBook(book)) {
+				return null;
+			}
+		}
+
+		return bkCart;
+	}
+
+	/**
+	 * @param BookCart
+	 * @return
+	 */
+	public ArrayList<StategyCombo> allUseAbleStrategyComboFromBookCart(
+			BookCart BookCart, ArrayList<Strategy> avalibleStrategies) {
+		int[] stategyCounts = new int[avalibleStrategies.size()];
+		int[] stategyMax = new int[avalibleStrategies.size()];
+
+		ArrayList<StategyCombo> stategyCombos = new ArrayList<StategyCombo>();
+		// 确定每种优惠策略使用的极值
+		for (int i = 0; i < avalibleStrategies.size(); i++) {
+			stategyMax[i] = BookCart.totalBooks
+					/ avalibleStrategies.get(i).bookCount;
+		}
+		int stategySize = avalibleStrategies.size();
+		while (true) {
+			// 整理数据(进位操作)
+			for (int i = 0; i < stategySize - 1; i++) {
+
+				if (stategyCounts[i] > stategyMax[i]) {
+					stategyCounts[i] = 0;
+					stategyCounts[i + 1] += 1;
+				}
+			}
+			// 循环退出点(最后一个数加满为结束点)
+			if (stategyCounts[stategySize - 1] > stategyMax[stategySize - 1]) {
+				break;
+			}
+			int totalBook = 0;
+
+			for (int i = 0; i < stategySize; i++) {
+				totalBook += avalibleStrategies.get(i).bookCount
+						* stategyCounts[i];
+			}
+			// 满足总数为8即为可能的优惠组合
+			if (totalBook == BookCart.totalBooks) {
+				StategyCombo stategyCombo = new StategyCombo();
+				for (int i = 0; i < avalibleStrategies.size(); i++) {
+					for (int j = 0; j < stategyCounts[i]; j++) {
+						stategyCombo.add(avalibleStrategies.get(i).copy());
+					}
+				}
+				stategyCombos.add(stategyCombo);
+			}
+
+			stategyCounts[0]++;
+		}
+		return stategyCombos;
+	}
+
+	/**
+	 * 对优惠组合进行排序
+	 * 
+	 * @param combos
+	 * @return
+	 */
+	public ArrayList<StategyCombo> sortStrategyCombos(
+			ArrayList<StategyCombo> combos) {
+		Collections.sort(combos, new Comparator<StategyCombo>() {
+
+			@Override
+			public int compare(StategyCombo o1, StategyCombo o2) {
+				// TODO Auto-generated method stub
+				if (o1.totalPrice > o2.totalPrice) {
+					return 1;
+				}
+				if (o1.totalPrice < o2.totalPrice) {
+					return -1;
+				}
+				return 0;
+			}
+		});
+		return combos;
+	}
+
+	/**
+	 * 获得最优惠的打包方式(可能不止一种)
+	 * 
+	 * @return
+	 */
+	public ArrayList<PackegeCombo> getTheCheapestPurchasePackegs(
+			BookCart BookCart, ArrayList<Strategy> avalibleStrategies) {
+		// 获得所有可能的优惠组合
+		ArrayList<StategyCombo> stragycombos = allUseAbleStrategyComboFromBookCart(
+				BookCart, avalibleStrategies);
+		// 对优惠组合进行排序
+		stragycombos = sortStrategyCombos(stragycombos);
+
+		ArrayList<PackegeCombo> packegeCombos = new ArrayList<PackegeCombo>();
+		for (int i = 0; i < stragycombos.size(); i++) {
+			// 查看此优惠组合是否可以打包成功
+			ArrayList<PackegeCombo> packegeCombos_i = pickAblePakegeComboWithStategyComboFromBookCart(
+					BookCart, stragycombos.get(i));
+			if (packegeCombos_i != null) {
+				packegeCombos.addAll(packegeCombos_i);
+			}
+			// 由于策略是按价格排序的 如果下一个策略的价格大于本策略且本策略已经找到结果 那么循环结束
+			if (i < stragycombos.size() - 1
+					&& packegeCombos.size() > 0
+					&& stragycombos.get(i + 1).totalPrice > stragycombos.get(i).totalPrice) {
+				break;
+			}
+
+		}
+		return packegeCombos;
+	}
+
+	/**
+	 * 获得最优惠的购书价格
+	 * 
+	 * @param BookCart
+	 * @param avalibleStrategies
+	 * @return
+	 */
+	public int returnTheCheepestPurchasePrice(BookCart BookCart,
+			ArrayList<Strategy> avalibleStrategies) {
+		ArrayList<PackegeCombo> packegeCombos = getTheCheapestPurchasePackegs(
+				BookCart, avalibleStrategies);
+		return getPricebyPackageCombo(packegeCombos);
+	}
+
+	public int getPricebyPackageCombo(ArrayList<PackegeCombo> packegeCombos) {
+		if (packegeCombos != null && packegeCombos.size() > 0) {
+			return packegeCombos.get(0).totalPrice;
+		}
+		return 0;
+	}
+
+}
---
+++ testKataPotter.java
@@ -0,0 +1,238 @@
+package CodeJam2008PracticeContest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import CodeJam2008PracticeContest.KataPotter.Book;
+import CodeJam2008PracticeContest.KataPotter.BookCart;
+import CodeJam2008PracticeContest.KataPotter.BookPackage;
+import CodeJam2008PracticeContest.KataPotter.PackegeCombo;
+import CodeJam2008PracticeContest.KataPotter.StategyCombo;
+import CodeJam2008PracticeContest.KataPotter.Strategy;
+
+public class testKataPotter {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	public BookCart getTestCart1() {
+		BookCart cart = new BookCart();
+		cart.addBook(new Book("A", 3));
+		cart.addBook(new Book("B", 3));
+		cart.addBook(new Book("C", 2));
+		cart.addBook(new Book("D", 1));
+		cart.addBook(new Book("E", 1));
+		return cart;
+	}
+	
+	public BookCart getTestCart2() {
+		BookCart cart = new BookCart();
+		cart.addBook(new Book("A", 2));
+		cart.addBook(new Book("B", 3));
+		cart.addBook(new Book("C", 1));
+		cart.addBook(new Book("D", 1));
+		cart.addBook(new Book("E", 1));
+		return cart;
+	}
+	
+	public BookCart getTestCart3() {
+		BookCart cart = new BookCart();
+		cart.addBook(new Book("A", 6));
+		cart.addBook(new Book("B", 6));
+		cart.addBook(new Book("C", 4));
+		cart.addBook(new Book("D", 2));
+		cart.addBook(new Book("E", 2));
+		return cart;
+	}
+	
+	
+
+	public ArrayList<Strategy> getTestStageries() {
+		ArrayList<Strategy> stragies = new ArrayList<KataPotter.Strategy>();
+		stragies.add(new Strategy(1, 100));
+		stragies.add(new Strategy(2, 95));
+		stragies.add(new Strategy(3, 90));
+		stragies.add(new Strategy(4, 80));
+		stragies.add(new Strategy(5, 75));
+		return stragies;
+	}
+	public void PrintCart(BookCart cart)
+	{
+		String ln="Cart:";
+		for(Book bk:cart.books)
+		{
+			ln=ln+bk.name+","+bk.count+":";
+		}
+		System.out.println(ln);
+	}
+	public void printStagertCombos(ArrayList<StategyCombo> stategyCombos) {
+		System.out.println("No.\ttotalprice\t stategray");
+		for (int i = 0; i < stategyCombos.size(); i++) {
+			String prtln = " ";
+			int price = 0;
+			for (Strategy stg : stategyCombos.get(i).strategies) {
+				prtln = prtln + stg.bookCount + " ";
+				price += stg.getStrategyPrice();
+			}
+			System.out.println(i + "\t:" + price * 8 + "\t " + prtln);
+		}
+	}
+	
+	public void printPackages(ArrayList<BookPackage> pkgs) {
+		System.out.println("printPkgs");
+		for(int j=0;j<pkgs.size();j++)
+		{
+			String prtln ="pkg"+j+":";
+			for(Book bk:pkgs.get(j).books)
+			{
+				prtln=prtln+bk.name+" ";
+			}
+			System.out.println(prtln);
+		}
+	}
+
+	public void printPackageCombos(ArrayList<PackegeCombo> pkgcmbs) {
+		for (int i = 0; i < pkgcmbs.size(); i++) {
+			System.out.println("packageCombo"+i);
+			
+			
+			for(int j=0;j<pkgcmbs.get(i).BookPackages.size();j++)
+			{
+				String prtln ="pkg"+j+":";
+				for(Book bk:pkgcmbs.get(i).BookPackages.get(j).books)
+				{
+					prtln=prtln+bk.name+" ";
+				}
+				System.out.println(prtln);
+			}
+		}
+	}
+	@Test
+	public void testKataPotter() {
+
+		KataPotter kataPotter = new KataPotter();
+		BookCart cart = getTestCart1();
+		PrintCart(cart);
+		ArrayList<Strategy> stragies = getTestStageries();
+		ArrayList<PackegeCombo> pkgcmbs = kataPotter
+				.getTheCheapestPurchasePackegs(cart, stragies);
+		assertTrue(pkgcmbs != null && pkgcmbs.size() != 0);
+		printPackageCombos(pkgcmbs);
+	}
+
+	@Test
+	public void testStatgeryBase() {
+
+		KataPotter kataPotter = new KataPotter();
+		BookCart cart = getTestCart1();
+		ArrayList<Strategy> stragies = getTestStageries();
+		ArrayList<StategyCombo> stategyCombos = kataPotter
+				.allUseAbleStrategyComboFromBookCart(cart, stragies);
+		assertNotNull(stategyCombos);
+		System.out.println("排序");
+		stategyCombos = kataPotter.sortStrategyCombos(stategyCombos);
+		printStagertCombos(stategyCombos);
+
+	}
+	
+	
+	/**
+	 * 第二套测试数据
+	 */
+	@Test
+	public void testKataPotter2() {
+		
+		KataPotter kataPotter = new KataPotter();
+		BookCart cart = getTestCart2();
+		PrintCart(cart);
+		ArrayList<Strategy> stragies = getTestStageries();
+		ArrayList<PackegeCombo> pkgcmbs = kataPotter
+				.getTheCheapestPurchasePackegs(cart, stragies);
+		assertTrue(pkgcmbs != null && pkgcmbs.size() != 0);
+		printPackageCombos(pkgcmbs);
+		assertEquals(pkgcmbs.get(0).totalPrice*8, 5320);
+	}
+	
+	/**
+	 * 第二套测试数据
+	 */
+	@Test
+	public void testKataPotter3() {
+		
+		KataPotter kataPotter = new KataPotter();
+		BookCart cart = getTestCart3();
+		PrintCart(cart);
+		ArrayList<Strategy> stragies = getTestStageries();
+		ArrayList<PackegeCombo> pkgcmbs = kataPotter
+				.getTheCheapestPurchasePackegs(cart, stragies);
+		assertTrue(pkgcmbs != null && pkgcmbs.size() != 0);
+		printPackageCombos(pkgcmbs);
+		
+		ArrayList<StategyCombo> stategyCombos = kataPotter
+				.allUseAbleStrategyComboFromBookCart(cart, stragies);
+		assertNotNull(stategyCombos);
+		System.out.println("排序");
+		stategyCombos = kataPotter.sortStrategyCombos(stategyCombos);
+		printStagertCombos(stategyCombos);
+		
+	}
+	
+	@Test
+	public void testPackagePick() {
+		KataPotter kataPotter = new KataPotter();
+		BookCart cart = getTestCart2();
+		PrintCart(cart);
+		ArrayList<BookPackage> pkgs= kataPotter.pickablePackegeFromBookCart(cart, 2);
+		printPackages(pkgs);
+		cart.pickOneBook(new Book("D", 1));
+		PrintCart(cart);
+		pkgs= kataPotter.pickablePackegeFromBookCart(cart, 2);
+		printPackages(pkgs);
+		
+	}
+	@Test
+	public void testStatgeryBase2() {
+
+		KataPotter kataPotter = new KataPotter();
+		BookCart cart = getTestCart2();
+		ArrayList<Strategy> stragies = getTestStageries();
+		ArrayList<StategyCombo> stategyCombos = kataPotter
+				.allUseAbleStrategyComboFromBookCart(cart, stragies);
+		assertNotNull(stategyCombos);
+		System.out.println("排序");
+		stategyCombos = kataPotter.sortStrategyCombos(stategyCombos);
+		printStagertCombos(stategyCombos);
+
+	}
+
+	@Test
+	public void testCartBaseOP() {
+
+		assertTrue("Not yet implemented", true);
+
+		KataPotter kataPotter = new KataPotter();
+		BookCart cart = getTestCart1();
+		assertEquals(cart.totalBooks, 10);
+		ArrayList<BookPackage> pkgs = kataPotter.pickablePackegeFromBookCart(
+				cart, 5);
+		assertEquals(pkgs.size(), 1);
+		cart = kataPotter.pickPackegeFromBookCart(cart, pkgs.get(0));
+		assertEquals(5, cart.totalBooks);
+		pkgs = kataPotter.pickablePackegeFromBookCart(cart, 5);
+		assertTrue(pkgs == null || pkgs.size() == 0);
+
+	}
+
+}
